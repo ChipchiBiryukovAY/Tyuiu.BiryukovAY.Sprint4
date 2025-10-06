5b28cabbabf3470bb54c1c1c18b4ded050035502
Алексей Бирюков <ChipichiX@yandex.ru>
2025-10-07 01:15:26 +0700

    Task1

--- Tyuiu.BiryukovAY.Sprint4.Task1.V5.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint4.Task1.V5.Lib/DataService.cs
@@ -2,19 +2,17 @@
 using tyuiu.cources.programming.interfaces.Sprint4;
 namespace Tyuiu.BiryukovAY.Sprint4.Task1.V5.Lib
 {
-    public class DataService : DataServiceBase, ISprint4Task1V5
+    public class DataService :  ISprint4Task1V5
     {
-        public override int Calculate(int[] array)
+        public int Calculate(int[] array)
         {
             int sum = 0;
-
-            foreach (int element in array)
+            foreach (int num in array)
             {
-                if (element % 2 == 0) 
-                {
-                    sum += element;
-                }
+                if (num % 2 == 0)
+                    sum += num;
             }
+            return sum;
         }
     }
 }
--- Tyuiu.BiryukovAY.Sprint4.Task1.V5.Test/DataServiceTest.cs
+++ Tyuiu.BiryukovAY.Sprint4.Task1.V5.Test/DataServiceTest.cs
@@ -10,9 +10,7 @@ namespace Tyuiu.BiryukovAY.Sprint4.Task1.V5.Test
         {
             DataService ds = new DataService();
             int[] array = { 9, 6, 6, 9, 7, 8, 5, 9, 6, 6, 8, 6, 8, 6, 7 };
-
             int result = ds.Calculate(array);
-
             Assert.AreEqual(54, result);
         }
     }
